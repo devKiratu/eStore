fe8da0ff2273f7f657c7ccb4c9506e458f8ca403
devKiratu <dev.kiratu@gmail.com>
2022-07-21 11:46:23 +0300

    refactor ProductDescription page

--- src/Pages/ProductDescription.js
+++ src/Pages/ProductDescription.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { gql } from "@apollo/client";
 import { Query } from "@apollo/client/react/components";
+import PdpDescriptions from "../components/PdpDescriptions";
 
 const LOAD_PRODUCT = gql`
 	query GetProduct($id: String!) {
@@ -42,23 +43,25 @@ export class ProductDescription extends Component {
 
 	render() {
 		const { id } = this.props.match.params;
-		console.log(this.props);
 		return (
 			<div className="pdp-container">
 				<Query query={LOAD_PRODUCT} variables={{ id }}>
 					{({ loading, error, data }) => {
 						if (loading) return <span>fetching product...</span>;
 						if (error) return <span>something went wrong :(</span>;
-						const { name, brand, inStock, gallery, attributes, description } =
-							data.product;
+						const {
+							name,
+							brand,
+							inStock,
+							gallery,
+							attributes,
+							description,
+							prices,
+						} = data.product;
 
-						console.log("in stock: ", inStock);
-						// console.log("gallery", gallery);
-						// console.log("attributes ", attributes);
-						const [currentPrice] = data.product.prices.filter(
+						const [currentPrice] = prices.filter(
 							(p) => p.currency.label === this.props.currency
 						);
-						// console.log("current price", currentPrice);
 						return (
 							<>
 								<div className="pdp-thumbnails">
@@ -67,14 +70,7 @@ export class ProductDescription extends Component {
 											key={index}
 											src={item}
 											alt={`${name} pic`}
-											style={{
-												width: "79px",
-												height: "80px",
-												objectFit: "contain",
-												// border: "1px solid gray",
-												marginBottom: "32px",
-												cursor: "pointer",
-											}}
+											className="pdp-image-thumbnail"
 											onClick={() => this.handleImageChange(item)}
 										/>
 									))}
@@ -83,68 +79,17 @@ export class ProductDescription extends Component {
 									<img
 										src={this.state.currentImage ?? gallery[0]}
 										alt={`${name} pic`}
-										style={{
-											width: "610px",
-											height: "511px",
-											objectFit: "contain",
-											// border: "1px solid cyan",
-										}}
+										className="pdp-current-image"
 									/>
 								</div>
-								<div className="pdp-descriptions">
-									<p className="brand-name">{brand}</p>
-									<p className="pdp-product-name">{name}</p>
-									{attributes.map((a, i) => (
-										<div key={i}>
-											<p className="pdp-attribute-name" key={a.name}>
-												{a.name}:
-											</p>
-											<div>
-												{a.items.map((i) =>
-													a.type === "swatch" ? (
-														<p
-															style={{
-																backgroundColor: `${i.value}`,
-																border: "0.5px solid gray",
-																width: "32px",
-																height: "32px",
-																margin: "8px 10px 36px 0",
-																display: "inline-block",
-															}}
-															key={i.displayValue}
-														></p>
-													) : (
-														<p
-															style={{
-																border: "1px solid gray",
-																width: "63px",
-																height: "45px",
-																margin: "8px 12px 24px 0",
-																display: "inline-flex",
-																justifyContent: "center",
-																alignItems: "center",
-															}}
-															key={i.displayValue}
-														>
-															{i.value}
-														</p>
-													)
-												)}
-											</div>
-										</div>
-									))}
-									<p className="pdp-attribute-name">price: </p>
-									<p className="pdp-price-details">
-										{currentPrice.currency.symbol} {currentPrice.amount}
-									</p>
-									<button className="pdp-add-button" disabled={!inStock}>
-										Add to cart
-									</button>
-									<div
-										className="pdp-innerhtml"
-										dangerouslySetInnerHTML={{ __html: description }}
-									/>
-								</div>
+								<PdpDescriptions
+									attributes={attributes}
+									brand={brand}
+									name={name}
+									currentPrice={currentPrice}
+									inStock={inStock}
+									description={description}
+								/>
 							</>
 						);
 					}}
---
+++ src/components/PdpDescriptions.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+export class PdpDescriptions extends Component {
+	render() {
+		const { attributes, brand, name, currentPrice, inStock, description } =
+			this.props;
+		return (
+			<div className="pdp-descriptions">
+				<p className="brand-name">{brand}</p>
+				<p className="pdp-product-name">{name}</p>
+				{attributes.map((a, i) => (
+					<div key={i}>
+						<p className="pdp-attribute-name" key={a.name}>
+							{a.name}:
+						</p>
+						<div>
+							{a.items.map((i) =>
+								a.type === "swatch" ? (
+									<p
+										className="attribute-type-swatch"
+										style={{ backgroundColor: `${i.value}` }}
+										key={i.displayValue}
+									></p>
+								) : (
+									<p className="attribute-type-text" key={i.displayValue}>
+										{i.value}
+									</p>
+								)
+							)}
+						</div>
+					</div>
+				))}
+				<p className="pdp-attribute-name">price: </p>
+				<p className="pdp-price-details">
+					{currentPrice.currency.symbol} {currentPrice.amount}
+				</p>
+				<button className="pdp-add-button" disabled={!inStock}>
+					Add to cart
+				</button>
+				<div
+					className="pdp-innerhtml"
+					dangerouslySetInnerHTML={{ __html: description }}
+				/>
+			</div>
+		);
+	}
+}
+
+export default PdpDescriptions;
